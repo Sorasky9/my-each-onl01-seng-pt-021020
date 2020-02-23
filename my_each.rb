b43183c6aa458039b71425bda547878cc85e9b3f
Malcom Fetus <sora.vista@gmail.com>
2020-02-23 00:15:14 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,8 +1,8 @@
 def my_each(words) # put argument(s) here
 words = ['hi', 'hello', 'bye', 'goodbye']
 i = 0
-my_each(words) while  i < words.length do |i|
-yield 
+my_each(words) while  i < words.length 
+yield
 i = i + 1
 
 
